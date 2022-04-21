8e1d05aab9386b3741a54a9e52e3b67c265ba6cc
suresh <sureshamahendra.gmail.com>
2022-04-21 21:58:51 +0530

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,50 @@
-/** @format */
-import "./App.css";
-import { Route, Routes, useNavigate } from "react-router-dom";
-import axios from "axios";
-import Nav from "./components/Nav";
-import Country from "./components/Country";
-import Weather from "./components/Weather";
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+import { Route, Routes, useNavigate } from 'react-router-dom';
+import PostTable from './component/PostTable';
+import Rawjson from './component/Rawjson';
 
-function App() {
+const App = () => {
+	const [data, setData]:any = useState<any>([]);
+	const [page, setPage]:any = useState(1)
+	let interval:NodeJS.Timer;
+	const navigate =useNavigate()
 
-	const navigate = useNavigate()
+	const fetchData = async()=>{
+		try{
+			const res = await axios.get(`https://hn.algolia.com/api/v1/search_by_date?query=story&page=${page}`)
+			console.log(res)
+			if(!data.includes([...res.data.hits])){
+				setData([...data, ...res.data.hits])
+			}
+		}
+		catch(e){
+			console.log(e);
+		}
+	}
 
-	return (
-	<>
-	<Nav navigate={navigate}/>
-	<Routes>
-		<Route path="/country/:country" element={<Country/>}/>
-		<Route path='/weather/:capital' element={<Weather/>}/>
-	</Routes>
-	</>
-	);
+	const handelPage = ()=>{
+		setPage((prev:any)=>prev+1)
+	}
+
+	useEffect(()=>{
+		console.log(page);
+		fetchData();
+		
+	},[page])
+
+	useEffect(()=>{
+		interval = setInterval(handelPage, 10000)
+	},[])
+
+  return (
+	<div>
+		<Routes>
+			<Route path='/' element={<PostTable data={data} setPage={setPage}/> }/>
+			<Route path='/rawjson' element={<Rawjson navigate={navigate}/>}/>
+		</Routes>
+	</div>
+  )
 }
 
-export default App;
+export default App
